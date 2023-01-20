f2eb9a9988a6bdd2de359405fa0b72bd5689b385
Dzmitry Tsiareshchanka <dima.tereshchenko.96@mail.ru>
2023-01-20 19:35:35 +0400

    feat: task finished"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const mapper = bracketsConfig.reduce((acc, bracketPair) => {
+    acc[bracketPair[1]] = bracketPair[0];
+    return acc;
+  }, {});
+
+  for (bracket of str) {
+    if (mapper[bracket] === bracket && !stack.includes(bracket)) {
+      stack.push(bracket);
+    }else if (Object.values(mapper).includes(bracket) && bracket !== mapper[bracket]) {
+      stack.push(bracket);
+    } else {
+      if(stack[stack.length - 1] === mapper[bracket]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
